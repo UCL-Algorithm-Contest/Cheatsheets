3d2f565e1a6d9e6760417eeaa3cb1634bea2dc08
Benoît Legat <benoit.legat@gmail.com>
2013-11-19 13:14:49 +0100

    Change KMP

---
+++ Formulaire/String/Kmp.java
@@ -0,0 +1,23 @@
+int[] kmpPreprocess(char[] p) {
+  int m = p.length;
+  int[] b = new int[m];
+  int i = 0, j = -1; b[0] = -1; // starting values
+  while (i < m) { // pre-process the pattern string p
+    while (j >= 0 && p[i] != p[j]) j = b[j]; // if different, reset j using b
+    i++; j++; // if same, advance both pointers
+    b[i] = j;
+  } }
+
+LinkedList<Integer> kmpSearch(char[] s, char[] p) { // text, pattern
+  int[] b = kmpPreprocess(p); // back table
+  int n = s.length, m = p.length;
+  LinkedList<Integer> found = new LinkedList<Integer>();
+  int i = 0, j = 0; // starting values
+  while (i < n) { // search through string s
+    while (j >= 0 && s[i] != p[j]) j = b[j]; // if different, reset j using b
+    i++; j++; // if same, advance both pointers
+    if (j == m) { // a match found when j == m
+      found.add(i-j);
+      j = b[j]; // prepare j for the next possible match
+    } }
+  return found; }
