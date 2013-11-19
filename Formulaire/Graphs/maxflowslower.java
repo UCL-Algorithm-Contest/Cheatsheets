f16002e1d850c183604ed48ab15ddceedb9cc018
fran.aubry@gmail.com <francois@fran-dell.(none)>
2013-11-19 09:48:09 +0100

    max flow short and fast (FF algo)

--- Formulaire/Graphs/maxflowslower.java
+++ Formulaire/Graphs/maxflowslower.java
@@ -1,6 +1,6 @@
-// Ford-Fulkerson (with slow DFS)
-static int pcap;
-static int maxflowFF(int[][] g, int source, int sink) {
+int pcap;
+
+int maxflowFF(TreeMap<Integer, Integer>[] g, int source, int sink) {
   int flow = 0;
   pcap = Integer.MAX_VALUE;
   while(augmentDFS(g, source, sink, new boolean[g.length])) {
@@ -9,57 +9,21 @@ static int maxflowFF(int[][] g, int source, int sink) {
   }
   return flow;
 }
-		
-static boolean augmentDFS(int[][] g, int cur, int sink, boolean[] done) {
+
+boolean augmentDFS(TreeMap<Integer, Integer>[] g, int cur, int sink, boolean[] done) {
   if(cur == sink) return true;
   if(done[cur]) return false;
   done[cur] = true;
-  for(int v = 0; v < g.length; v++) {
-    if(g[cur][v] > 0) {
-      pcap = Math.min(pcap, g[cur][v]);
-      if(augmentDFS(g, v, sink, done)) {
-        g[cur][v] -= pcap;
-        g[v][cur] += pcap;
+  for(Entry<Integer, Integer> e : g[cur].entrySet()) {
+    if(e.getValue() > 0) {
+      pcap = Math.min(pcap, e.getValue());
+      if(augmentDFS(g, e.getKey(), sink, done)) {
+        g[cur].put(e.getKey(), e.getValue() - pcap);
+        Integer backcap = g[e.getKey()].get(cur);
+        g[e.getKey()].put(cur, backcap == null? pcap : backcap + pcap);
         return true;
       }
     }
   }
   return false;
-}
-
-// Edmonds-Karp (with slow BFS)
-static int maxflowEK(int[][] g, int source, int sink) {
-  int flow = 0;
-  int pcap;
-  while((pcap = augmentBFS(g, source, sink)) != -1)
-    flow += pcap;
-  return flow;
-}
-
-static int augmentBFS(int[][] g, int source, int sink) {
-  Queue<Integer> Q = new LinkedList<Integer>();
-  Integer[] p = new Integer[g.length];
-  int[] pcap = new int[g.length];
-  pcap[source] = Integer.MAX_VALUE;
-  p[source] = -1;
-  Q.add(source);
-  while(p[sink] == null && !Q.isEmpty()) {
-    int cur = Q.poll();
-    for(int v = 0; v < g.length; v++) {
-      if(g[cur][v] > 0 && p[v] == null) {
-        p[v] = cur;
-        pcap[v] = Math.min(pcap[cur], g[cur][v]);
-        Q.add(v);
-      }
-    }
-  }
-  if(p[sink] == null) return -1;
-  // update residual graph
-  int cur = sink;
-  while(cur != source) {
-    g[p[cur]][cur] -= pcap[sink];
-    g[cur][p[cur]] += pcap[sink];
-    cur = p[cur];
-  }
-  return pcap[sink];
 }
